fb885a3ad8151d763b64535beff5105d101fa023
Jackson Karel <jackson.karel2@gmail.com>
2022-01-06 20:01:34 -0800

    Segment TOE episode sentences

--- preproc.py
+++ preproc.py
@@ -1,23 +1,30 @@
 import re
 
+from punctuation import segment_sents
+
 
 class Clean_text:
-    def __init__(self, input_file, output_file, sub_pattern, substitute):
+    def __init__(self, input_file, output_file, sub_pattern, substitute, seg_sents):
         self.input_file = input_file
         self.output_file = output_file
         self.sub_pattern = sub_pattern
         self.substitute = substitute
+        self.seg_sents = seg_sents
 
     def txt_write(self):
         with open(self.input_file) as f:
             text = f.read()
-        proc_text = re.sub(self.sub_pattern, self.substitute, text)
+        if self.seg_sents is True:
+            text_sents = segment_sents(text, newlines=True)
+            proc_text = '\n'.join(text_sents)
+        else:
+            proc_text = re.sub(self.sub_pattern, self.substitute, text)
         
         with open(self.output_file, 'w') as f:
             f.write(proc_text)
 
-clean_youtube_auto = Clean_text("unpunct.txt", "unpunct_clean.txt", "(\n| )\[.*?\](\n| )", " ")
-clean_toe = Clean_text("unpunct_toe.txt", "unpunct_toe_clean.txt", "(CJ|LE)( |-|:)*", "")
+clean_youtube_auto = Clean_text("unpunct.txt", "unpunct_clean.txt", "(\n| )\[.*?\](\n| )", " ", False)
+clean_toe = Clean_text("unpunct_toe.txt", "unpunct_toe_clean.txt", "(CJ|LE)( |-|:)*", "", False)
+seg_toe = Clean_text("unpunct_toe_clean.txt", "punct_test.txt", None, None, True)
 
-# clean_toe.txt_write()
-
+# seg_toe.txt_write()
