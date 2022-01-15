061f280f255b268e8fcdbe7d29d30f34000c21be
Jackson Karel <jackson.karel2@gmail.com>
2022-01-15 13:16:15 -0800

    Fix created_utc log and remove unnecessary spaces from dataset

--- ufonovl/punctuation.py
+++ ufonovl/punctuation.py
@@ -1,8 +1,17 @@
 import spacy
+import re
 
 def segment_sents(text, newlines):
     nlp = spacy.load("en_core_web_lg", exclude=["ner", "parser", "tagger", "lemmatizer"])
     nlp.enable_pipe("senter")
     doc = nlp(text)
-    text_sents = [sent.text.replace('\n', newlines) if newlines else sent.text for sent in doc.sents]
+    text_sents = []
+    for sent in doc.sents:
+        if newlines:
+            sent_text = sent.text.replace('\n', newlines)
+        else:
+            sent_text = sent.text
+        sent_text = re.sub("^ ", "", sent_text)
+        text_sents.append(sent_text)
+        
     return text_sents
--- ufonovl/reddit/history.py
+++ ufonovl/reddit/history.py
@@ -8,18 +8,19 @@ def reddit_history():
     # UFOs subreddit created Mon Mar 10 2008 PDT (1205211399)
     # aliens subreddit created 1217838855
     # HighStrangeness subreddit created 1238686810
-    start_epoch = 1205211399
+    start_epoch = 1238686810
     epochs = [start_epoch]
     for ep in epochs:
         p_shift_api = PushshiftAPI()
 
         submissions = list(p_shift_api.search_submissions(after=ep,
                                     sort='asc',
-                                    subreddit='UFOs',
+                                    subreddit='HighStrangeness',
                                     filter=['title', 'selftext', 'id'],
                                     limit=100))
         reddit_subs_dn(submissions)
         last_sub_created = submissions[-1].created_utc
+        lsc_str = str(last_sub_created)
         with open("logs/time.txt", 'w') as f:
-            f.write(last_sub_created)
+            f.write(lsc_str)
         epochs.append(last_sub_created)
